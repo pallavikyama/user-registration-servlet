79a23001235d9b4c2bf151a005f92c675514190a
pallavikyama <kyamap98@gmail.com>
2020-12-15 18:32:11 +0530

    Added: Created a sample servlet class and configured tomcat-server.

---
+++ src/main/java/com/myfirstservlet/SimpleServlet.java
@@ -0,0 +1,23 @@
+package com.myfirstservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Hello")
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Welcome Pallavi. This is your first Servlet!</h3>");
+		out.close();
+	}
+}
